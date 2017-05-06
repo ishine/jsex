355f422ee2beabd7584a35e36249865b1e151d05
Vikman Fdez-Castro <vmfdez90@gmail.com>
2017-05-07 01:00:35 +0200

    Implement query test

--- src/main.c
+++ src/main.c
@@ -9,8 +9,19 @@ int main(int argc, char **argv) {
     char * delimiter;
     size_t linecap;
     jsex_t * jsex;
+    cJSON * json;
 
-    if (argc == 1) {
+    if (argc < 2) {
+        fprintf(stderr, "ERROR: No JSON given.\nSyntax: %s <JSON> [ <query> ]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    if (json = cJSON_Parse(argv[1]), !json) {
+        fprintf(stderr, "ERROR: Parsing input JSON.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (argc == 2) {
         while (getline(&line, &linecap, stdin) >= 0) {
             delimiter = strchr(line, '\n');
 
@@ -19,17 +30,18 @@ int main(int argc, char **argv) {
             }
 
             if (jsex = jsex_parse(line), jsex) {
+                printf("Result: %s\n", jsex_exec(jsex, json) ? "True" : "False");
                 jsex_free(jsex);
-                printf("\n");
             } else {
                 printf("JSex parser error.\n\n");
             }
         }
     } else {
-        for (i = 1; i < argc; i++) {
+        for (i = 2; i < argc; i++) {
             printf("\nParsing: %s\n", argv[i]);
 
             if (jsex = jsex_parse(argv[i]), jsex) {
+                printf("Result: %s\n", jsex_exec(jsex, json) ? "True" : "False");
                 jsex_free(jsex);
             } else {
                 printf("JSex parser error.\n");
